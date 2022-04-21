46ce50673dec7f77dbe628f382f8da0f965e2d82
haeliseu <arisalexia3@gmail.com>
2022-04-21 10:39:43 -0400

    les pays sont affichés mais il n'y a plus de css

--- index-afrique.php
+++ index-afrique.php
@@ -5,29 +5,39 @@ include('inc/connect_db.php');
 extract($_GET); 
 ?>
 
-<div class="ui container">
+<!DOCTYPE html>
+<html>
+<body>
+    <div class="ui container">
     <center>
     <h1>Les Pays d'Afrique</h1>
     </center>
-<div>
-<table>
-
-<?php
-$stmt = $pdo->prepare("SELECT * FROM country WHERE Continent = 'Africa'");
-$stmt->execute();
-$ligne = $stmt->fetch();
-
-    for($i=0;$i<=58;$i++){ 
-    echo"<tr>";
-    $name= $ligne["Name"];
-    echo"</tr>";
-    echo"<form method='GET' action='pays.php'><tr><td><input type='submit' name='name' value='$name' style='border-radius: 25px; background-color: white;' name='$name'></input></td><td></form>";
-}
-?>
-</table>
+ <table>
+   <thead>
+     <tr>
+       <th>Nom des pays</th>
+     </tr>
+   </thead>
+   <tbody>
+     <?php 
+     $stmt = $pdo->prepare("SELECT * FROM country WHERE Continent = 'Africa'");
+     $stmt->execute();
+     while($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+     <tr>
+       <td><?php echo htmlspecialchars($row['Name']); ?></td>
+     </tr>
+     <!-- echo"<form method='GET' action='pays.php'><tr><td><input type='submit' name='name' value='$name' style='border-radius: 25px; background-color: white;' name='$name'></input></td><td></form>";
+} -->
+     <?php endwhile; ?>
+   </tbody>
+ </table>
+</body>
+</html>
 </div>
 <p></p>
 <?php
 require_once 'javascripts.php';
 require_once 'footer.php';
 ?>
+
+
